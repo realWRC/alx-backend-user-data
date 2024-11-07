e922e0a4ff3d6f244e33fc7d562141565259a6c1
Wongani Chulu <wonganichulu@gmail.com>
2024-11-07 18:03:53 +0200

    Task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -39,7 +39,8 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-    return mysql.connector.connection.MySQLConnection(
+
+    return mysql.connector.connect(
         user=username,
         password=password,
         host=host,
@@ -66,7 +67,3 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
-
-
-if __name__ == '__main__':
-    pass
