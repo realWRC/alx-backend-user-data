f7978fc866634f44e3982388fcd42b5221f63c25
Wongani Chulu <wonganichulu@gmail.com>
2024-11-07 17:48:34 +0200

    Task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,7 +24,7 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
 
     stream_handler = logging.StreamHandler()
-    formatter = RedactingFormatter(fields=PII_FIELDS)
+    formatter = RedactingFormatter(fields=list(PII_FIELDS))
     stream_handler.setFormatter(formatter)
 
     logger.addHandler(stream_handler)
