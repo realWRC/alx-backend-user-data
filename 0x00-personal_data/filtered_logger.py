b934d94f9a84a0a7bb06e68ef4abe9acdc2466c8
Wongani Chulu <wonganichulu@gmail.com>
2024-11-07 17:03:58 +0200

    Added fix 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,11 @@ Obfuscation functions for user data
 """
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Obfuscates user data in a logfile"""
     pattern = r'(' + '|'.join(fields) + r')=([^' + separator + r']*)'
     return re.sub(pattern, r'\1=' + redaction, message)
