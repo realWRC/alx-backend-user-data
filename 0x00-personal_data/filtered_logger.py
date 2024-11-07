34606d035d60d6af51cf96319119137ed98e3b11
Wongani Chulu <wonganichulu@gmail.com>
2024-11-07 18:01:09 +0200

    Task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,8 @@ Obfuscation functions for user data
 
 import re
 import logging
+import os
+import mysql.connector
 from typing import List
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -29,6 +31,20 @@ def get_logger() -> logging.Logger:
 
     logger.addHandler(stream_handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connects to the MySQL database"""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    return mysql.connector.connection.MySQLConnection(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )
 
 
 class RedactingFormatter(logging.Formatter):
