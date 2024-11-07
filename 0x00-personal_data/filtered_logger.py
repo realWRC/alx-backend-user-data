c20629858fb851d8060ff376bd4b267220a2aa5a
Wongani Chulu <wonganichulu@gmail.com>
2024-11-07 17:24:36 +0200

    Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,3 +34,7 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == '__main__':
+    pass
