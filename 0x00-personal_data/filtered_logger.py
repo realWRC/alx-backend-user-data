e586e58e21f031761c3260f40013ab3760efb238
Wongani Chulu <wonganichulu@gmail.com>
2024-11-07 17:21:33 +0200

    Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,6 +31,6 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         """Formats the log"""
-        record.msg = filter_datum(
-            self.fields,self.REDACTION,record.getMessage(), self.SEPARATOR)
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
