ee07107389f4466e6ee5c417f173ed7014ba0d41
Wongani Chulu <wonganichulu@gmail.com>
2024-11-07 21:07:03 +0200

    Task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,9 @@ def hash_password(password: str) -> bytes:
     password_bytes = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a password is valid"""
+    password_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_password)
