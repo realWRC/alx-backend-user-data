898c1cf33e028d94abd06586079e9de968de3f7f
Wongani Chulu <wonganichulu@gmail.com>
2024-11-07 17:38:03 +0200

    Task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,12 +7,28 @@ import re
 import logging
 from typing import List
 
+PII_FIELDS = ["name", "email", "phone", "ssn", "password"]
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Obfuscates user data in a logfile"""
     pattern = r'(' + '|'.join(fields) + r')=([^' + separator + r']*)'
     return re.sub(pattern, r'\1=' + redaction, message)
+
+
+def get_logger() -> logging.Logger:
+    """Creates configured logger"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
