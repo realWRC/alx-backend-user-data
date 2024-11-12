b4c66aa7e4f613a9cb6fed8e5ddebe8db9dbe97b
Wongani Chulu <wonganichulu@gmail.com>
2024-11-12 06:36:28 +0200

    Added def extract_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,20 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic authentication class
     """
-    pass
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """ Returns the Base64 part of the Authorization header
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        header = authorization_header.split(' ')[-1]
+        return header
