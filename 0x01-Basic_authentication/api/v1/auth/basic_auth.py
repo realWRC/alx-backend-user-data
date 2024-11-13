cbf84c10984acb07b75daeb57663fb59949a9f03
Wongani Chulu <wonganichulu@gmail.com>
2024-11-13 02:53:16 +0200

    Improving extract_user_credentials and require_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -59,7 +59,6 @@ class BasicAuth(Auth):
         if not isinstance(decoded_base64_authorization_header, str):
             return None, None
 
-        # comments
         if ':' not in decoded_base64_authorization_header:
             return None, None
 
