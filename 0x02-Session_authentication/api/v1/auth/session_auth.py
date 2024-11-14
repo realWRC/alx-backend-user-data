f87a3ec37090639e3875971a6f5a5e430942feb4
Wongani Chulu <wonganichulu@gmail.com>
2024-11-14 19:56:07 +0200

    Added created SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" A session authentication implimentation
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ A class for session authentication
+    """
+    pass
