ae2bfb44d99ea947519a0a82bd761c544de9cdda
Wongani Chulu <wonganichulu@gmail.com>
2024-11-19 20:26:44 +0200

    Fixed db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,10 +31,10 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> Union[User, None]:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ Creates a user based on email and password
         """
         newUser = User(email=email, hashed_password=hashed_password)
-        self.__session.add(newUser)
-        self.__session.commit()
+        self._session.add(newUser)
+        self._session.commit()
         return newUser
