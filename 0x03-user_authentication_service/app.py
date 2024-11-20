954b9bb5944f2e28a4644491d246800e698fdb1c
Wongani Chulu <wonganichulu@gmail.com>
2024-11-20 21:51:37 +0200

    Added pwd reset route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -81,5 +81,20 @@ def profile() -> Union[Tuple[Response, Literal[int]], Response, None]:
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token() -> Union[Tuple[Response, Literal[int]], Response, None]:
+    """ Generates and gives a reset token
+    """
+    try:
+        email = request.form.get('email')
+        if not email:
+            return abort(403)
+
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        return abort(403)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
