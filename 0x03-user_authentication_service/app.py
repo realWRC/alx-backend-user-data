8cf3426523a53bf43635025d186647a705dae0e0
Wongani Chulu <wonganichulu@gmail.com>
2024-11-20 21:12:00 +0200

    Updated logout endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,16 +56,16 @@ def login() -> Union[Tuple[Response, Literal[int]], Response, None]:
 def logout() -> Union[Response, None]:
     """ User logout end-point
     """
-    try:
-        session_id = request.cookies.get("session_id")
-        user = AUTH.get_user_from_session_id(session_id)
-        if user:
-            AUTH.destroy_session(user.id)
-            redirect('/')
-    except Exception:
-        pass
-    else:
+    session_id = request.cookies.get("session_id")
+    if not session_id:
         return abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        return abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == '__main__':
