32ef5be66907c13309cdbb459de3ea478c2dfe09
Wongani Chulu <wonganichulu@gmail.com>
2024-11-20 15:46:44 +0200

    Fix for task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,7 +34,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> Union[User, None]:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ Creates a user based on email and password
         """
         user = User()
