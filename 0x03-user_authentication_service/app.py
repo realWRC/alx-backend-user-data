b56c4213561defeb5ee6636abe224a8dfe4b97e1
Wongani Chulu <wonganichulu@gmail.com>
2024-11-20 17:28:07 +0200

    Added register user endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,14 @@
 """ A simple flask app.
 """
 
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
+from sqlalchemy import Integer
 from auth import Auth
+from typing import Tuple, Union
+from typing_extensions import Literal
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -13,6 +17,21 @@ def index() -> Response:
     """ General default
     """
     return jsonify({"message": "Bienvenue"})
+
+
+# def register() -> Union[Tuple[Response, Literal[int]], Response]:
+@app.route('/users', methods=['POST'])
+def register() -> Union[Tuple[Response, Literal[int]], Response]:
+    """ Registers a user
+    """
+    try:
+        user = AUTH.register_user(
+            email=request.form.get('email'),
+            password=request.form.get('password')
+        )
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
