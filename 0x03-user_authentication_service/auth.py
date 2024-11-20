bb0ac396cdd3c17edf182669c22e60a607c14f7d
Wongani Chulu <wonganichulu@gmail.com>
2024-11-20 22:14:45 +0200

    Added update password functionality

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -100,3 +100,16 @@ class Auth:
             raise ValueError
         user.reset_token = _generate_uuid()
         return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Updates user deats with token and password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        self._db.update_user(
+            user.id,
+            hashed_password=_hash_password(password),
+            reset_token = None
+        )
