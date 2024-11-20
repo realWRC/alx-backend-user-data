8541fa93cf9a840a2e9fc679a386e8ed325bf852
Wongani Chulu <wonganichulu@gmail.com>
2024-11-20 20:47:30 +0200

    Updated app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """ A simple flask app.
 """
 
-from flask import Flask, jsonify, Response, request, abort
+from flask import Flask, jsonify, Response, redirect, request, abort
 from auth import Auth
 from typing import Tuple, Union
 from typing_extensions import Literal
@@ -53,17 +53,16 @@ def login() -> Union[Tuple[Response, Literal[int]], Response, None]:
 
 
 @app.route('/sessions', methods=['DELETE'])
-def logout() -> Union[Tuple[Response, Literal[int]], Response, None]:
+def logout() -> Union[Response, None]:
     """ User logout end-point
     """
-    try:
-        user = AUTH.get_user_from_session_id(request.args.get('session_id'))
-        if user:
-            AUTH.destroy_session(user.id)
-    except Exception:
-        pass
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
     else:
-        return abort(403)
+        abort(403)
 
 
 if __name__ == '__main__':
