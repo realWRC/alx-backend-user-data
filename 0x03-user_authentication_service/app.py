45732dffa8846700d042003de889218111b8fea3
Wongani Chulu <wonganichulu@gmail.com>
2024-11-20 21:02:14 +0200

    Added logout fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -61,8 +61,9 @@ def logout() -> Union[Response, None]:
         user = AUTH.get_user_from_session_id(session_id)
         if user:
             AUTH.destroy_session(user.id)
-            return redirect("/")
     except Exception:
+        pass
+    else:
         return abort(403)
 
 
